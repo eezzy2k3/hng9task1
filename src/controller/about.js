6168b8e5965bd6888d0e26e694a12a9e5032d8d7
Gbose(eezzy) <eezzy2k3@yahoo.com>
2022-11-04 18:50:17 +0100

    update

--- src/controller/about.js
+++ src/controller/about.js
@@ -12,10 +12,60 @@ const about = async(req,res)=>{
 }
 
 const operations = async(req,res)=>{
-    const x = req.body.x
-    const y = req.body.y
-    const operation_type =  req.body.operation_type
+    let x = req.body.x
+    let y = req.body.y
+    let operation_type =  req.body.operation_type
+
+   
+
+    let numb = operation_type.match(/\d/g);
+    
+    if(numb && numb.length == 2){
+        y=Number(numb[0])
+        x=Number(numb[1])
+    }
+     
+    if(numb && numb.length > 2 ){
+        console.log(numb)
+       const newy = numb.slice(0,2)
+       console.log(newy)
+       const newx = numb.slice(2,4)
+       console.log(newx)
+
+       y = Number(newy.join(""))
+       console.log(y)
+       x = Number(newx.join(""))
+       console.log(x)
+  }
+    
+    
   const  operation_types = ["addition","subtraction","multiplication"]
+    
+  if(operation_type.includes("add")){
+      operation_type = "addition"
+  }
+  if(operation_type.includes("sum")){
+    operation_type = "addition"
+}
+if(operation_type.includes("plus")){
+    operation_type = "addition"
+}
+  if(operation_type.includes("subtract")){
+    operation_type = "subtraction"
+  }
+  if(operation_type.includes("takeaway")){
+    operation_type = "subtraction"
+  }
+  if(operation_type.includes("minus")){
+    operation_type = "subtraction"}
+  if(operation_type.includes("multiply")){
+    operation_type = "multiplication"
+  }if(operation_type.includes("product")){
+    operation_type = "multiplication"}
+    if(operation_type.includes("times")){
+        operation_type = "multiplication"}
+    
+    
     if(!operation_types.includes(operation_type)) return res.status(400).json({success:false,msg:"invalid operation type"})
     let result;
     if(operation_type === "addition"){
@@ -26,9 +76,10 @@ const operations = async(req,res)=>{
         result = x*y
     }
 
-    await res.status(201).json({slackUsername:"eezzy2k3",operation_type:operation_type,result:result})
+    await res.status(200).json({slackUsername:"eezzy2k3",operation_type:operation_type,result:result})
 
     
 }
 
 module.exports = {about,operations}
+
